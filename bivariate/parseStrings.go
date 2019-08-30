8688e860e512021e61b11845738028eef1626b11
René B. Christensen <rene@math.aau.dk>
2019-08-30 15:32:25 +0200

    Regexp

--- bivariate/parseStrings.go
+++ bivariate/parseStrings.go
@@ -1,56 +1,30 @@
 package bivariate
 
 import (
-	"bytes"
 	"fmt"
 	"regexp"
 	"strconv"
 )
 
-func byteToDegreeAndCoef(b []byte) (degs [2]uint, coef uint, err error) {
-	xDeg := regexp.MustCompile(`(?i:x)\^?([0-9]*)`).FindAll(b, -1)
-	yDeg := regexp.MustCompile(`(?i:x)\^?([0-9]*)`).FindAll(b, -1)
-	c := regexp.MustCompile(`^(-?[0-9])`).Find(b)
-	if len(xDeg) > 1 || len(yDeg) > 1 {
-		err = fmt.Errorf("Parsing monomial %s: Found several X's or Y's", b)
-		return
-	}
-	if len(xDeg) == 1 {
-		tmp, errX := strconv.ParseUint(string(xDeg[0]), 10, 0)
-		if errX != nil {
-			degs[0] = uint(tmp)
-		}
-	} else {
-		degs[0] = 0
-	}
-	if len(yDeg) == 1 {
-		tmp, errY := strconv.ParseUint(string(yDeg[0]), 10, 0)
-		if errY != nil {
-			degs[1] = uint(tmp)
-		}
-	} else {
-		degs[1] = 0
-	}
-	if len(c) == 0 {
-		coef = 1
-	} else {
-		tmp, errCoef := strconv.ParseUint(string(c[0]), 10, 0)
-		if errCoef != nil {
-			coef = uint(tmp)
-		}
-	}
+func byteToDegreeAndCoef(string) (degs [2]uint, coef uint, err error) {
+
 	return
 }
 
 func polynomialStringToMap(s string) (map[[2]uint]uint, error) {
-	b := []byte(s)
-	b = bytes.ReplaceAll(b, []byte(" "), []byte(""))
-	monomials := bytes.FieldsFunc(b, func(r rune) bool {
-		if r == rune('+') || r == rune('-') {
-			return true
-		}
-		return false
-	})
+	matches := regexp.MustCompile(
+		`\s*(?P<sign>\+|-)\s*`+
+			`(?P<coef>[0-9]*)\s*\*?\s*`+
+			`(?P<var1>(?i:x|y))?\^?(?P<deg1>[0-9]*)\s*\*?\s*`+
+			`(?P<var2>(?i:x|y))?\^?(?P<deg2>[0-9]*)\s*`).FindAllStringSubmatch(s, -1)
+	// Check that total match length is the full input string
+	matchLen := 0
+	for _, m := range matches {
+		matchLen += len(m[0])
+	}
+	if matchLen != len(s) {
+		return nil, fmt.Errorf("Failed to parse string %s as polynomial", s)
+	}
 	out := make(map[[2]uint]uint)
 	for _, m := range monomials {
 		deg, coef, err := byteToDegreeAndCoef(m)
