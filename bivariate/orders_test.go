ea1d8506279c29771ac0b9da0098f4efcd948860
René B. Christensen <rene@math.aau.dk>
2019-09-16 18:24:25 +0200

    Add testing of monomial orders

---
+++ bivariate/orders_test.go
@@ -0,0 +1,51 @@
+package bivariate
+
+import (
+	"testing"
+)
+
+func TestOrders(t *testing.T) {
+	degrees := [][2][2]uint{
+		{{1, 1}, {1, 1}},
+		{{2, 1}, {1, 1}},
+		{{2, 1}, {1, 2}},
+		{{0, 4}, {1, 0}},
+		{{4, 3}, {3, 4}},
+		{{4, 0}, {0, 3}},
+	}
+	orders := []Order{
+		Lex(true),
+		Lex(false),
+		DegLex(true),
+		DegLex(false),
+		WDegLex(3, 4, true),
+		WDegLex(3, 4, false),
+	}
+	ordStr := []string{
+		"Lex (X>Y)",
+		"Lex (Y>X)",
+		"DegLex (X>Y)",
+		"DegLex (Y>X)",
+		"WDegLex(3,4) (X>Y)",
+		"WDegLex(3,4) (Y>X)",
+	}
+	expectedOrd := [][]int{ // First entry X>Y; second entry Y>X
+		{0, 0, 0, 0, 0, 0},
+		{1, 1, 1, 1, 1, 1},
+		{1, -1, 1, -1, -1, -1},
+		{-1, 1, 1, 1, 1, 1},
+		{1, -1, 1, -1, -1, -1},
+		{1, -1, 1, 1, 1, -1},
+	}
+
+	for i, d := range degrees {
+		for j, o := range orders {
+			if res := o(d[0], d[1]); res != expectedOrd[i][j] {
+				t.Errorf(
+					"Wrong ordering of %v and %v using %s (got %d, expected %d)",
+					d[0], d[1], ordStr[j], res, expectedOrd[i][j],
+				)
+			}
+		}
+	}
+}
