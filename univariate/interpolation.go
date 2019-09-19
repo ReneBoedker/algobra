e712223042e40e0f59a95a98f8348787836cf50a
René B. Christensen <rene@math.aau.dk>
2019-09-19 11:08:26 +0200

    Add interpolation support for univariate polynomials

---
+++ univariate/interpolation.go
@@ -0,0 +1,76 @@
+package univariate
+
+import (
+	"algobra/errors"
+	"algobra/finitefield"
+)
+
+// Interpolate computes the Lagrange interpolation polynomial evaluating to
+// values in the specified points. The resulting polynomial has degree at most
+// len(points).
+//
+// It returns an InputValue-error if the number of points and values differ, or
+// if points are not distinct.
+func (r *QuotientRing) Interpolate(
+	points []*finitefield.Element,
+	values []*finitefield.Element,
+) (*Polynomial, error) {
+	const op = "Computing interpolation"
+
+	if len(points) != len(values) {
+		return nil, errors.New(
+			op, errors.InputValue,
+			"Different number of interpolation points and values (%d and %d)",
+			len(points), len(values),
+		)
+	}
+
+	if !allDistinct(points) {
+		return nil, errors.New(
+			op, errors.InputValue,
+			"Interpolation points must me distinct",
+		)
+	}
+
+	f := r.Zero()
+	for i := range points {
+		f = f.Plus(r.lagrangeBasis(points, i).Scale(values[i]))
+	}
+	if f.Err() != nil {
+		panic(f.Err())
+	}
+
+	return f, nil
+}
+
+// allDistinct checks if given points are all distinct
+func allDistinct(points []*finitefield.Element) bool {
+	unique := make(map[string]struct{})
+	for _, p := range points {
+		if _, ok := unique[p.String()]; ok {
+			return false
+		}
+		unique[p.String()] = struct{}{}
+	}
+	return true
+}
+
+// lagrangeBasis computes a "lagrange-type" basis element. That is, it computes
+// a polynomial that evaluates to 1 in point at index and to 0 in any other
+// point of points.
+func (r *QuotientRing) lagrangeBasis(points []*finitefield.Element, index int) *Polynomial {
+	f := r.PolynomialFromUnsigned([]uint{1})
+
+	for i, p := range points {
+		if i == index {
+			continue
+		}
+
+		f = f.Mult(r.Polynomial([]*finitefield.Element{
+			p.Neg(),
+			r.baseField.One(),
+		})).Scale(points[index].Minus(p).Inv())
+	}
+
+	return f
+}
---
+++ univariate/interpolation_test.go
@@ -0,0 +1,130 @@
+package univariate
+
+import (
+	"algobra/errors"
+	"algobra/finitefield"
+	"testing"
+)
+
+func TestLagrangeBasis(t *testing.T) {
+	field := defineField(17, t)
+	ring := DefRing(field)
+
+	for rep := 0; rep < 100; rep++ {
+		const nPoints = 4
+		points := make([]*finitefield.Element, nPoints, nPoints)
+
+		nRuns := 0
+		for nRuns == 0 || !allDistinct(points) {
+			for j := 0; j < nPoints; j++ {
+				points[j] = field.ElementFromUnsigned(uint(prg.Uint32()))
+			}
+			nRuns++
+			if nRuns >= 100 {
+				t.Log("Skipping generation after 100 attempts")
+				break
+			}
+		}
+
+		for j := range points {
+			// f evaluates to 1 in points[j] and 0 in other components of 0
+			f := ring.lagrangeBasis(points, j)
+
+			if f.Zero() {
+				t.Errorf("Lagrange basis is zero with points %v and index %d",
+					points, j)
+			} else if ld := f.Ld(); ld > (nPoints - 1) {
+				t.Errorf("Lagrange basis has too large total degree (%d) with point %v",
+					ld, points[j],
+				)
+			}
+
+			for k, p := range points {
+				ev := f.Eval(p)
+				switch {
+				case j == k && !ev.One():
+					t.Errorf("f(%v)=%v instead of 1 with f = %v", p, ev, f)
+				case j != k && ev.Nonzero():
+					t.Errorf("f(%v)=%v instead of 0 with f = %v", p, ev, f)
+				}
+			}
+		}
+	}
+}
+
+func TestInterpolation(t *testing.T) {
+	field := defineField(23, t)
+	ring := DefRing(field)
+	for rep := 0; rep < 100; rep++ {
+		const nPoints = 4
+		points := make([]*finitefield.Element, nPoints, nPoints)
+
+		nRuns := 0
+		for nRuns == 0 || !allDistinct(points) {
+			for j := 0; j < nPoints; j++ {
+				points[j] = field.ElementFromUnsigned(uint(prg.Uint32()))
+			}
+			nRuns++
+			if nRuns >= 100 {
+				t.Log("Skipping generation after 100 attempts")
+				break
+			}
+		}
+
+		values := make([]*finitefield.Element, nPoints, nPoints)
+		for i := 0; i < nPoints; i++ {
+			values[i] = field.ElementFromUnsigned(uint(prg.Uint32()))
+		}
+
+		f, err := ring.Interpolate(points, values)
+
+		if err != nil {
+			t.Errorf("Interpolation returned error: %q", err)
+		} else {
+			// Test that all evaluations are correct
+			var testVals [nPoints]*finitefield.Element
+			overAllSuccess := true
+			for i, p := range points {
+				testVals[i] = f.Eval(p)
+				overAllSuccess = overAllSuccess && testVals[i].Equal(values[i])
+			}
+			if !overAllSuccess {
+				t.Errorf(
+					"Interpolation failed for points = %v and values = %v.\n"+
+						"(Returned polynomial %v with values %v)",
+					points, values, f, testVals,
+				)
+			}
+		}
+	}
+}
+
+func TestInterpolationErrors(t *testing.T) {
+	field := defineField(13, t)
+	ring := DefRing(field)
+
+	a := field.Zero()
+	b := field.ElementFromUnsigned(5)
+
+	_, err := ring.Interpolate(
+		[]*finitefield.Element{a, b},
+		[]*finitefield.Element{field.Zero()})
+	if err == nil {
+		t.Errorf("Interpolation did not return an error even though there " +
+			"were more points than values")
+	} else if !errors.Is(errors.InputValue, err) {
+		t.Errorf("Interpolation returned an error on different length inputs, "+
+			"but it was of unexpected kind (err = %v)", err)
+	}
+
+	_, err = ring.Interpolate(
+		[]*finitefield.Element{a, a},
+		[]*finitefield.Element{field.Zero(), field.Zero()})
+	if err == nil {
+		t.Errorf("Interpolation did not return an error even though input " +
+			"contains duplicate points")
+	} else if !errors.Is(errors.InputValue, err) {
+		t.Errorf("Interpolation returned an error when input contains duplicate"+
+			" points, but it was of unexpected kind (err = %v)", err)
+	}
+}
