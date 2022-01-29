d466810123bf485693d504f6d164dbbb5082facd
René B. Christensen <rene@math.aau.dk>
2022-01-29 15:08:17 +0100

    Avoid multiple Pow-calls in Eval
    New implementation is faster for polynomials with many terms. The
    older is likely to be faster for sufficiently sparse polynomials.

--- univariate/benchmark_test.go
+++ univariate/benchmark_test.go
@@ -42,3 +42,31 @@ func BenchmarkStringExtension(b *testing.B) {
 	field, _ := finitefield.Define(49)
 	benchString(field, b)
 }
+
+func benchEval(field ff.Field, b *testing.B) {
+	ring := univariate.DefRing(field)
+
+	for rep := 0; rep < b.N; rep++ {
+		for i := 0; i < 100; i++ {
+			// Create random polynomial with up to 50 different terms
+			nDegs := (uint(prg.Uint32()) % 50) + 1
+			coefs := make([]ff.Element, nDegs, nDegs)
+			for i := uint(0); i < nDegs; i++ {
+				coefs[i] = field.RandElement()
+			}
+			f := ring.Polynomial(coefs)
+
+			f.Eval(field.RandElement())
+		}
+	}
+}
+
+func BenchmarkEvalBinfield(b *testing.B) {
+	field, _ := finitefield.Define(256)
+	benchEval(field, b)
+}
+
+func BenchmarkEvalPrime(b *testing.B) {
+	field, _ := finitefield.Define(113)
+	benchEval(field, b)
+}
